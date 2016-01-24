acb3b412308007d2628464b1ada3d563618c9ee4
michimawan <openpublick@gmail.com>
2016-01-24 23:00:23 +0700

    adding migration for user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,14 +14,20 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
             $table->string('email')->unique();
+            $table->string('displayName');
+            $table->string('firstName');
+            $table->string('lastName')->nullable();
+            $table->string('photo');
+            $table->enum('source', ['facebook', 'google', 'internal'])->default('internal');
+            $table->enum('gender', ['male', 'female'])->default('male');
             $table->string('password', 60);
+            $table->boolean('status')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
     }
-
+    
     /**
      * Reverse the migrations.
      *
